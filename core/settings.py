47137ee11b0cbae2f9f6998fe78c8292107f9d58
karamfarhan <www.karam777krm@gmail.com>
2022-08-05 16:43:57 +0300

    try to fix static files

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,6 +5,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("game.urls", namespace="game")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
