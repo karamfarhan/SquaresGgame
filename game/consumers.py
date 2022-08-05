23ad304ee9e1006bbf813c862de0556345740c52
karamfarhan <www.karam777krm@gmail.com>
2022-08-05 12:00:13 +0300

    test precommit

--- game/consumers.py
+++ game/consumers.py
@@ -3,44 +3,39 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.cache import cache
 
-from .game_handlers import add_player_to_game, creat_game, d
+from .game_handlers import add_player_to_game, creat_game, generate_game_id
 
 
 class GameConsumer(AsyncWebsocketConsumer):
-    ## conect
+    # conect
     async def connect(self):
-        ## join the group
+        # join the group
         self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
         await self.channel_layer.group_add(
             self.game_id,
             self.channel_name,
         )
-
         game = await cache.aget(f"game:{self.game_id}")
         if len(game["players"].keys()) >= game["start_at_player"]:
             game["is_started"] = True
-            await self.channel_layer.group_send(
-                self.game_id, {"type": "Send_Game", "data": game}
-            )
+            await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": game})
         else:
             players = game["players"]
-            await self.channel_layer.group_send(
-                self.game_id, {"type": "Update_Players", "data": players}
-            )
+            await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": players})
 
         await self.accept()
 
-    ## disconnect
+    # disconnect
     async def disconnect(self, close_code):
-        ## Leave room group
+        # Leave room group
         await self.channel_layer.group_discard(self.game_id, self.channel_name)
 
-    ## receive message
+    # receive message
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         data = text_data_json["data"]
 
-        ## Send message to room group
+        # Send message to room group
         if text_data_json["method"] == "update_ball":
             game = await cache.aget(f"game:{self.game_id}")
             game["squares"][f'{data["squareId"]}']["color"] = data["color"]
@@ -64,37 +59,31 @@ class GameConsumer(AsyncWebsocketConsumer):
             await cache.aset(f"game:{self.game_id}", game)
             if len(game["players"].keys()) >= game["start_at_player"]:
                 game["is_started"] = True
-                await self.channel_layer.group_send(
-                    self.game_id, {"type": "Send_Game", "data": game}
-                )
+                await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": game})
             else:
                 players = game["players"]
-                await self.channel_layer.group_send(
-                    self.game_id, {"type": "Update_Players", "data": players}
-                )
+                await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": players})
 
     async def Send_Game(self, event):
         data = event["data"]
 
-        ## Send message to WebSocket
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({"method": "send_game", "data": data}))
 
     async def Update_Square(self, event):
         data = event["data"]
 
-        ## Send message to WebSocket
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({"method": "update_square", "data": data}))
 
     async def Update_Players(self, event):
         data = event["data"]
 
-        ## Send message to WebSocket
-        await self.send(
-            text_data=json.dumps({"method": "update_players", "data": data})
-        )
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"method": "update_players", "data": data}))
 
     async def Send_Results(self, event):
         data = event["data"]
 
-        ## Send message to WebSocket
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({"method": "send_results", "data": data}))
