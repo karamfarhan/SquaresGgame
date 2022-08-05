a453ebaf6d9512548ceb79cc4baf013615e4b2e6
karamfarhan <www.karam777krm@gmail.com>
2022-08-05 17:20:31 +0300

    try to fix static files

--- core/settings.py
+++ core/settings.py
@@ -102,8 +102,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join("static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = []
 
 
