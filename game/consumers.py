7c5dc0d8d62786e02203527ab0779d7829e65f57
karamfarhan <www.karam777krm@gmail.com>
2022-08-08 16:02:54 +0300

    Remove player when leave and fixing waiting area
    - when player leave game, he will be removed from the players
    - the other users can see how left
    - fix the waiting are, show it just when the game is not started

--- game/consumers.py
+++ game/consumers.py
@@ -11,26 +11,41 @@ class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # join the group
         self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
+        self.player_name = self.scope["url_route"]["kwargs"]["name"]
         await self.channel_layer.group_add(
             self.game_id,
             self.channel_name,
         )
         game = await cache.aget(f"game:{self.game_id}")
-        if len(game["players"].keys()) >= game["start_at_player"]:
-            if game["is_started"] is False:
-                game["is_started"] = True
-                await cache.aset(f"game:{self.game_id}", game)
-                await self.channel_layer.group_send(self.game_id, {"type": "Start_Timer", "data": "start the time"})
-            await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": game})
-        else:
-            players = game["players"]
-            await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": players})
+        if game:
+            if len(game["players"].keys()) >= game["start_at_player"]:
+                if game["is_started"] is False:
+                    game["is_started"] = True
+                    await cache.aset(f"game:{self.game_id}", game)
+                    await self.channel_layer.group_send(
+                        self.game_id, {"type": "Start_Timer", "data": "start the time"}
+                    )
+                await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": game})
+            else:
+                waiting = False
+                if game["is_started"] is False:
+                    waiting = True
+                data = {"players": game["players"], "waiting": waiting}
+                await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": data})
 
         await self.accept()
 
     # disconnect
     async def disconnect(self, close_code):
         # Leave room group
+        game = await cache.aget(f"game:{self.game_id}")
+        try:
+            del game["players"][self.player_name]
+        except Exception:
+            pass
+        await cache.aset(f"game:{self.game_id}", game)
+        data = {"players": game["players"], "waiting": False}
+        await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": data})
         await self.channel_layer.group_discard(self.game_id, self.channel_name)
 
     # receive message
@@ -72,8 +87,11 @@ class GameConsumer(AsyncWebsocketConsumer):
                     )
                 await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": game})
             else:
-                players = game["players"]
-                await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": players})
+                waiting = False
+                if game["is_started"] is False:
+                    waiting = True
+                data = {"players": game["players"], "waiting": waiting}
+                await self.channel_layer.group_send(self.game_id, {"type": "Update_Players", "data": data})
 
     async def Send_Game(self, event):
         data = event["data"]
--- game/routing.py
+++ game/routing.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    path("ws/game/<str:game_id>/", consumers.GameConsumer.as_asgi()),
+    re_path(r"ws/game/(?P<game_id>\w+)/(?P<name>\w+)/$", consumers.GameConsumer.as_asgi()),
+    # path("ws/game/<str:game_id>/", consumers.GameConsumer.as_asgi()),
 ]
